bdf54b71a85f76c9ab0d13ab05e713036a00f2ab
MJ Rossetti <s2t2@users.noreply.github.com>
2022-08-31 15:00:55 -0400

    Trendlines and colors in ggplot

--- scripts/class_1_mpg.r
+++ scripts/class_1_mpg.r
@@ -11,13 +11,33 @@ library(ggplot2)
 # GG PLOT
 # https://ggplot2.tidyverse.org/
 # https://github.com/rstudio/cheatsheets/blob/main/data-visualization-2.1.pdf
-
+# trend line: https://www.statology.org/ggplot-trendline/
 
 df <- ggplot2::mpg
 
 print(df)
 
+#df %>%
+#  ggplot(aes(
+#      x = displ, 
+#      y = hwy,
+#      color = class
+#  )) +
+#  geom_point() +
+#  geom_smooth() +
+#  theme_classic()
+#
+
+
+
 df %>%
-  ggplot(aes(x = displ, y = hwy)) +
-  geom_point() +
+  ggplot(
+    mapping = aes(x = displ, y = hwy)
+  ) +
+  geom_point(
+    mapping = aes(color = class)
+  ) +
+  geom_smooth(
+    #se = FALSE
+  ) +
   theme_classic()
