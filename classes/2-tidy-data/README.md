774e353befc493327855d42bdfbf9d21b2a57668
MJ Rossetti <s2t2@users.noreply.github.com>
2022-09-07 14:33:18 -0400

    Solutions for class 2 exercises - pivots

---
+++ classes/2-tidy-data/README.md
@@ -0,0 +1,3 @@
+
+
+source: https://eda.seas.gwu.edu/2022-Fall/hw/1-tidy-data.html
---
+++ scripts/hw_1.r
@@ -0,0 +1,19 @@
+# https://eda.seas.gwu.edu/2022-Fall/hw/1-tidy-data.html
+
+# https://cfss.uchicago.edu/notes/tidy-exercise/
+
+# usethis::use_course("uc-cfss/data-wrangling-tidy-data")
+
+# https://rdrr.io/github/uc-cfss/rcfss/
+install.packages("remotes")
+remotes::install_github("uc-cfss/rcfss")
+
+
+library(tidyverse)
+
+# library(rcfss) #> Error in library(rcfss) : there is no package called ‘rcfss’
+# library("uc-cfss/rcfss")
+
+#print(race) #> object 'race' not found
+
+print(race)
