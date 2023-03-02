bc932742f9169572bf44c9e0f4650ed63301dbd4
MJ Rossetti <s2t2@users.noreply.github.com>
2023-03-02 14:39:21 -0500

    API example

--- scripts/scraping.R
+++ scripts/scraping.R
@@ -13,7 +13,7 @@ library(rvest)
 
 
 
-# 1) Intro to html (most people don't know the syntax) & how to parse html for data. 
+# 1) Intro to html (most people don't know the syntax) & how to parse html for data.
 # ... Need an example of a simple, static page with a nice table of data to scrape out of it.
 # ... https://raw.githubusercontent.com/prof-rossetti/intro-to-python/main/data/my_lists.html
 # ... https://raw.githubusercontent.com/prof-rossetti/intro-to-python/main/data/my_tables.html
@@ -23,15 +23,15 @@ library(rvest)
 lists_page <- read_html("https://raw.githubusercontent.com/prof-rossetti/intro-to-python/main/data/my_lists.html")
 
 # GET ALL MATCHING ELEMENTS:
-# flavors_list <- lists_page %>% html_elements("ol") 
+# flavors_list <- lists_page %>% html_elements("ol")
 #> xml_nodeset
 
 # GET FIRST MATCHING ELEMENT:
-# flavors_list <- lists_page %>% html_element("ol") 
+# flavors_list <- lists_page %>% html_element("ol")
 #> html_node
 
 # GET FIRST ELEMENT BY ID
-flavors_list <- lists_page %>% html_node("#my-fav-flavors") 
+flavors_list <- lists_page %>% html_node("#my-fav-flavors")
 #> html_node
 
 # CHILDREN / TRAVERSING
@@ -52,11 +52,11 @@ cat(flavors[3], "\n")
 tables_page <- read_html("https://raw.githubusercontent.com/prof-rossetti/intro-to-python/main/data/my_tables.html")
 
 # GET FIRST MATCHING ELEMENT:
-# products_table <- tables_page %>% html_element("table") 
+# products_table <- tables_page %>% html_element("table")
 #> html_node
 
 # GET FIRST ELEMENT BY ID
-products_table <- tables_page %>% html_node("#products") 
+products_table <- tables_page %>% html_node("#products")
 #> html_node
 
 # TABLE MAGIC (NEED TO KNOW ABOUT DATAFRAMES)
@@ -68,7 +68,7 @@ products_table <- tables_page %>% html_node("#products")
 # product_rows <- products_table %>% html_children()
 
 # GET SPECIFIC CHILD ELEMENTS
-product_rows <- products_table %>% html_elements("tr") 
+product_rows <- products_table %>% html_elements("tr")
 
 #products <- c() # tried this, but can't store a vector in a vector, so using a list instead
 products_list <- list()
@@ -81,7 +81,7 @@ for (row in product_rows[-1]) {
     product_id <- cells[1] %>% html_text() %>% readr::parse_integer()
     product_name <- cells[2] %>% html_text()
     product_price <- cells[3] %>% html_text() %>% readr::parse_double()
-    
+
     # assemble our own named vector (i.e. dictionary):
     product <- c("id"=product_id, "name"=product_name, "price"=product_price)
     cat(product, "\n")
@@ -95,18 +95,110 @@ for (row in product_rows[-1]) {
 
 cat("\n", products)
 
-# 2) Scraping data from multiple pages. 
-# ... Need an example where you have to loop through pages and extract the data on each one. 
+
+# 1C)
+# http://bulletin.gwu.edu/courses/emse/
+
+emse_page <- read_html("http://bulletin.gwu.edu/courses/emse/")
+
+course_divs <- emse_page %>% html_nodes(".courseblock")
+
+for(div in course_divs){
+    cat("----------------------", "\n")
+    title_block <- div %>% html_node(".courseblocktitle")  %>% html_text()
+    #> EMSE 6845. Lean and Agile Systems Engineering. 3 Credits.
+    cat(title_block, "\n")
+
+    desc_block <- div %>% html_node(".courseblockdesc") %>% html_text()
+    #> Lean and agile methods as applied to the engineering design and development of systems; review of contemporary implementation frameworks, methodologies, and the tools used to support them. Implications for traditional systems engineering; fundamental changes to the requirements processes; implications for engineering management. Prerequisite:
+    cat(desc_block, "\n")
+
+}
+
+
+
+# 2) Scraping data from multiple pages.
+# ... Need an example where you have to loop through pages and extract the data on each one.
 # ... Best if it's a simple static site where you can  just change the url to get a new page
 # ... https://github.com/s2t2/gwu-courses-py
 # ... https://my.gwu.edu/mod/pws/courses.cfm?campId={self.campus_id}&termId={self.term_id}&subjId={self.subject_id}
 # ... https://my.gwu.edu/mod/pws/courses.cfm?campId=1&termId=202301&subjId=EMSE
 # ... JK gw courses pagination requires a click (:-/)
-
 # ...
+# ... https://www.gutenberg.org/ebooks/author/65 # SHAKESPEARE
+# ... https://www.gutenberg.org/ebooks/author/50533 # HEMMINGWAY
+# ... JK not very interesting
+#
+# ... GW BULLETIN IS MAYBE BETTER:
+# http://bulletin.gwu.edu/courses/emse/
+# http://bulletin.gwu.edu/courses/csci/
+
+subjects <- c("EMSE", "CSCI")
+
+for(subject in subjects){
+    cat("----------------------", "\n")
+    cat(subject)
+
+    page <- read_html(paste("http://bulletin.gwu.edu/courses/", tolower(subject), sep=""))
+    course_divs <- page %>% html_nodes(".courseblock")
+
+    for(div in course_divs){
+        cat("---", "\n")
+        title_block <- div %>% html_node(".courseblocktitle")  %>% html_text()
+        #> EMSE 6845. Lean and Agile Systems Engineering. 3 Credits.
+        cat(title_block, "\n")
+        # TODO: split on period, get course_id, course_title, n_credits
+
+        desc_block <- div %>% html_node(".courseblockdesc") %>% html_text()
+        #> Lean and agile methods as applied to the engineering design and development of systems; review of contemporary implementation frameworks, methodologies, and the tools used to support them. Implications for traditional systems engineering; fundamental changes to the requirements processes; implications for engineering management. Prerequisite:
+        cat(desc_block, "\n")
+
+    }
+
+}
 
 # 3) Simple API example (if there's time).
+# ...
+# ... see stocks.R script if the data is in CSV format
+#
+# ... otherwise if in JSON format, can use httr
+# ... https://www.rdocumentation.org/packages/httr/versions/1.4.5
+# ... https://www.rdocumentation.org/packages/httr/versions/1.4.5/topics/GET
 
 
+install.packages("httr")
 
+# https://www.thecocktaildb.com/api.php
+# https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic
+#> {"drinks": [{}, {}, ... {}]}
 
+drinks_response <- httr::GET(url="https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
+cat(typeof(drinks_response), "\n")
+
+drinks_data <- drinks_response %>% httr::content()
+cat(typeof(drinks_data), "\n")
+cat(names(drinks_data), "\n")
+#> named list with "drinks" key
+
+drinks_list <- drinks_data["drinks"]
+cat(typeof(drinks_list), "\n")
+cat(length(drinks_list), "\n") #> why 1?
+
+drinks <- drinks_list[[1]]
+cat(typeof(drinks), "\n")
+cat(length(drinks), "\n") #> 58
+
+for (drink in drinks[1:5]){
+    #print(drink) #> list
+    d <- drink[[1]]
+    #> make a named vector (i.e. dictionary) so we can actually reference the keys
+    #drink_id <- drink["idDrink"]
+    #drink_name <- drink["strDrink"]
+    #drink_image_url <- d["strDrinkThumb"]
+    # ARG OK, the values are lists too? SO ANNOYING...
+    drink_id <- drink["idDrink"][[1]]
+    drink_name <- drink["strDrink"][[1]]
+    drink_image_url <- d["strDrinkThumb"][[1]]
+
+    cat(drink_id, drink_name, "\n")
+}
