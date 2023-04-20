5f0198b6d76976792014074f1104c4cf7f4d5b26
Mike Rossetti <s2t2@users.noreply.github.com>
2023-04-20 13:40:17 -0400

    Update scrape_quotes.R

--- scripts/scrape_quotes.R
+++ scripts/scrape_quotes.R
@@ -9,12 +9,19 @@ request_url = "https://quotes.toscrape.com"
 page = read_html(request_url)
 quotes = page %>% html_nodes(".quote")
 
-# for a given node:
-quote_text = quotes[[1]] %>% html_nodes(".text") %>% html_text()
-author_name = quotes[[1]] %>% html_nodes(".author")  %>% html_text()
-author_path <- quotes[[1]] %>% html_element("a") %>% html_attr("href")
-author_url <- paste(request_url, author_path, sep="")
+# for a given quote:
+#quote_text = quotes[[1]] %>% html_nodes(".text") %>% html_text()
+#author_name = quotes[[1]] %>% html_nodes(".author")  %>% html_text()
+#author_path <- quotes[[1]] %>% html_element("a") %>% html_attr("href")
+#author_url <- paste(request_url, author_path, sep="")
+#cat(quote_text, "\n -", author_name, "\n", author_url)
 
-cat(quote_text, "\n -", author_name, "\n", author_url)
-
-
+# for all quotes:
+for(quote in quotes){
+    quote_text = quote %>% html_nodes(".text") %>% html_text()
+    author_name = quote %>% html_nodes(".author")  %>% html_text()
+    author_path <- quote %>% html_element("a") %>% html_attr("href")
+    author_url <- paste(request_url, author_path, sep="")
+    cat("\n--------------------\n")
+    cat(quote_text, "\n -", author_name, "...", author_url)
+}
