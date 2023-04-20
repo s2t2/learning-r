517b70d9aec9de21495e29d939e3c54dfae95c1e
Mike Rossetti <s2t2@users.noreply.github.com>
2023-04-20 13:46:34 -0400

    Update scrape_courses.R

--- scripts/scrape_courses.R
+++ scripts/scrape_courses.R
@@ -16,17 +16,19 @@ for(subject in subjects){
     
     request_url <- paste("http://bulletin.gwu.edu/courses/", tolower(subject), sep="")
     page <- read_html(request_url)
-    course_divs <- page %>% html_nodes(".courseblock")
     
+    course_divs <- page %>% html_nodes(".courseblock")
     for(div in course_divs){
         cat("---", "\n")
-        title_block <- div %>% html_node(".courseblocktitle")  %>% html_text()
-        #> EMSE 6845. Lean and Agile Systems Engineering. 3 Credits.
-        cat(title_block, "\n")
-        # TODO: split on period, get course_id, course_title, n_credits
         
+        title_block <- div %>% html_node(".courseblocktitle")  %>% html_text()
+        cat(title_block, "\n")
+        #> EMSE 6845. Lean and Agile Systems Engineering. 3 Credits.
+        # TODO: split on period, get course_id, course_title, n_credits
+
         desc_block <- div %>% html_node(".courseblockdesc") %>% html_text()
-        #> Lean and agile methods as applied to the engineering design and development of systems; review of contemporary implementation frameworks, methodologies, and the tools used to support them. Implications for traditional systems engineering; fundamental changes to the requirements processes; implications for engineering management. Prerequisite:
         cat(desc_block, "\n")
+        #> Lean and agile methods as applied to the engineering design and development of systems; review of contemporary implementation frameworks, methodologies, and the tools used to support them. Implications for traditional systems engineering; fundamental changes to the requirements processes; implications for engineering management. Prerequisite:
+
     } 
 }
